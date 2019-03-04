16d7ccd2cf506637433747172791dd7b277613d8
B. Kessels <b.kessels@veldhuis.invalid>
2019-03-04 08:00:00 +0000

    test: attribution of a plain day shift

---
+++ test/attribution.test.ts
@@ -0,0 +1,16 @@
+import { attributionDay } from "../src/rules/attribution";
+import { ALDERVALE } from "../src/contracts/aldervale";
+import type { Shift } from "../src/types";
+
+// Fixture times are wall-clock strings, the way the schedulers write them.
+function shift(start: string, end: string): Shift {
+  return { start: start, end: end };
+}
+
+// The rota, the payslip and the export all stand on this one answer.
+describe("attributionDay", () => {
+  it("attributes a day shift to that day", () => {
+    const s = shift("2026-03-03T07:00", "2026-03-03T19:00");
+    expect(attributionDay(s, ALDERVALE)).toBe("2026-03-03");
+  });
+});
